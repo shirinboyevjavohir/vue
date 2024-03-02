e703eaed2693b157adb97ba449c629cb45a06d75
javokhir0905 <shirinboyevjavokhir@gamil.com>
2024-03-02 16:54:30 +0500

    movie-app loyihasi uchun componentlarni create qildim

---
+++ todoApp/src/components/app/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app">
+    <div class="content">
+      <AppInfo />
+      <div class="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <MovieList />
+      <MovieAddForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInfo from "../app-info/AppInfo.vue";
+import SearchPanel from "../search-panel/SearchPanel.vue";
+import AppFilter from "../app-filter/AppFilter.vue";
+import MovieList from "../movie-list/MovieList.vue";
+import MovieAddForm from "../movie-add-form/MovieAddForm.vue";
+export default {
+  components: {
+    AppInfo,
+    SearchPanel,
+    AppFilter,
+    MovieList,
+    MovieAddForm,
+  },
+};
+</script>
+
+<style>
+.app {
+  height: 100vh;
+  color: #000;
+}
+
+.content {
+  width: 80%;
+  min-height: 700px;
+  background: #fff;
+  padding: 5rem 0;
+  margin: 0 auto;
+  border: 1px solid red;
+}
+
+.search-panel {
+  margin-top: 2rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+</style>
