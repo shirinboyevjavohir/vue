5a04a974164aa9e35b5a63b0c1c6affa1778dcac
javokhir0905 <shirinboyevjavokhir@gamil.com>
2024-03-24 12:25:16 +0500

    #2 Navbar and routers are ready

---
+++ java-vue-app/src/constant/index.js
@@ -0,0 +1,2 @@
+export const logo =
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Flovepik.com%2Fimages%2Fpng-game-logo.html&psig=AOvVaw0g72u0X_N1hbKvQe6KHvyB&ust=1711348230907000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjVpY2jjIUDFQAAAAAdAAAAABAF";
---
+++ java-vue-app/src/views/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from "./HomeView.vue";
+export { default as Login } from "./LoginView.vue";
+export { default as Register } from "./RegisterView.vue";
