3cf446164be33f523eb92302aca16fbb2fe68c31
javokhir0905 <shirinboyevjavokhir@gamil.com>
2024-03-27 22:19:46 +0500

    VueX bilan ishlash

--- java-vue-app/src/views/HomeView.vue
+++ java-vue-app/src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>Home page</div>
+  <div>
+    Home page {{ counter }}
+    <div class="w-25">
+      <Button @click="incrementHandler">Increase</Button>
+    </div>
+    <div class="w-25">
+      <Button @click="decrementtHandler">Decrement</Button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    incrementHandler() {
+      this.$store.commit("increment");
+    },
+    decrementtHandler() {
+      this.$store.commit("decrement");
+    },
+  },
+  computed: {
+    counter() {
+      return this.$store.state.count;
+    },
+  },
+};
 </script>
 
 <style></style>
